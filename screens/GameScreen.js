eb287dab752d79754a1ff6fb574f2303ffa698ff
Avery Uslaner <avery.uslaner@redbutte.utah.edu>
2023-01-05 11:10:19 -0700

    State after lesson 62. Title Component

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,9 +1,10 @@
 import { Text, View, StyleSheet } from "react-native";
+import Title from "../components/Title";
 
 function GameScreen() {
   return (
     <View style={styles.screen}>
-      <Text>Game Screen!</Text>
+      <Title>Opponent's Guess</Title>
       {/* GUESS */}
       <View>
         <Text>Higher or lower?</Text>
@@ -21,5 +22,5 @@ const styles = StyleSheet.create({
     screen: {
         flex: 1,
         padding: 24
-    }
+    },
 });
